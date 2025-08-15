3993cb62881c43437591c70527f2c0da9e861e7d
Mathieu7483 <11436@holbertonstudents.com>
2025-08-15 10:29:56 +0200

    Refactor execute_command and main functions for improved readability and consistency

--- shell_v0.1.c
+++ shell_v0.1.c
@@ -1,38 +1,37 @@
 #include "simple_shell.h"
 /**
- * execute_command - create child and execute programm
- * @command: command to execute
- * @envp: environment variables
+* execute_command - create child and execute programm
+* @command: command to execute
+* @envp: environment variables
 */
 void execute_command(char *command, char **envp)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();/** create child */
-    if (pid == -1)
-	{
-        perror("fork");/**error if child doesn't works */
-        return;
-    }
+	pid = fork();/** create child */
 
-    if (pid == 0)
+	if (pid == -1)
 	{
-        char *argv[2];
-        argv[0] = command;
-        argv[1] = NULL;
+		perror("fork");/**error if child doesn't works */
+		return;
+	}
+	if (pid == 0)
+	{
+		char *argv[2];
+
+		argv[0] = command;
+		argv[1] = NULL;
 		/* Execute the command with arguments and environment */
 		execve(command, argv, envp);
-
 		perror(command);/* Display error message if execve fails */
-        exit(1);
-    }
+		exit(1);
+	}
 	else
 	{
 		waitpid(pid, &status, 0);/* Wait for child process to complete */
-    }
+	}
 }
-
 /**
 *main - entry point for the simple shell program
 *@argc: number of arguments
@@ -42,53 +41,44 @@ void execute_command(char *command, char **envp)
 */
 int main(int argc, char **argv, char **envp)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    
-    (void)argc;/*ignore argc and argv for the gcc*/
-    (void)argv;
+	char *line = NULL;
 
-    while (1)
+	size_t len = 0;
+	ssize_t nread;
+	(void)argc;/*ignore argc and argv for the gcc*/
+	(void)argv;
+	while (1)
 	{
-        printf("#cisfun$ ");/*our prompt Cisfun$ */
-        fflush(stdout);/*free the memory buffered*/
-
+		printf("#cisfun$ ");/*our prompt Cisfun$ */
+		fflush(stdout);/*free the memory buffered*/
 		nread = getline(&line, &len, stdin);/* Read user input from stdin */
-
-        if (nread == -1) 
+		if (nread == -1)
 		{
-            if (feof(stdin))/*if not read exit program*/
+			if (feof(stdin))/*if not read exit program*/
 			{
-
-                printf("\n");
-                break;
-            }
+				printf("\n");
+				break;
+			}
 			else
 			{
-
-                perror("getline");/*display error on getline and quit*/
-                exit(1);
-            }
-        }
-		
+				perror("getline");/*display error on getline and quit*/
+				exit(1);
+			}
+		}
 		if (line[nread - 1] == '\n')
 		{
-			line[nread - 1] = '\0';/*Remove the newline character at the end of the input*/
+			line[nread - 1] = '\0';
+			/*Remove the newline character at the end of the input*/
 		}
-
-        if (strlen(line) > 0)
+		if (strlen(line) > 0)
 		{
-
-			
 			if (strcmp(line, "exit") == 0)/*Check if user wants to exit the shell*/
 			{
-                break;
-            }
-			execute_command(line, envp); /*Execute the user command with environment variables*/
-        }
-    }
-
-    free(line);
-    return (0);
+				break;
+			}
+			execute_command(line, envp);/*Execute the user command*/
+		}
+	}
+	free(line);
+	return (0);
 }
