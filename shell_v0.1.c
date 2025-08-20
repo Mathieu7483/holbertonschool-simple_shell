fd2c19042c2634ccf7ad2328594239ad5567a58e
Mathieu7483 <11436@holbertonstudents.com>
2025-08-20 15:32:50 +0200

    Refactor main and run_shell functions in shell_v0.1.c for improved clarity and structure

--- shell_v0.1.c
+++ shell_v0.1.c
@@ -1,3 +1,4 @@
+#include "simple_shell.h"
 /**
 * execute_command - create child and execute program
 * @command: command to execute
@@ -32,52 +33,70 @@ void execute_command(char *command, char **envp)
 	}
 }
 /**
-* main - entry point for the simple shell program
-* @argc: number of arguments
-* @argv: array of argument strings
-* @envp: array of environment variables
-*
-* Return: 0 on success, 1 on error
-*/
+ * main - Entry point for the simple shell program
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ * @envp: Array of environment variables
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char **argv, char **envp)
+{
+	simple_shell_t shell_state;
+	(void)argc, (void)argv;
+
+	shell_state.is_interactive = isatty(STDIN_FILENO);
+	shell_state.exit_status = 0;
+
+	run_shell(&shell_state, envp);
+
+	return (shell_state.exit_status);
+}
+
+/**
+ * run_shell - The main loop of the shell
+ * @shell_state: Pointer to the shell's state structure
+ * @envp: Array of environment variables
+ */
+void run_shell(simple_shell_t *shell_state, char **envp)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
 	char *args[256];
-	(void)argc, (void)argv;
+
 	while (1)
 	{
-		printf("cisfun$ "); /* display the prompt */
+		if (shell_state->is_interactive)
+			printf("$ ");
 		fflush(stdout);
-		nread = getline(&line, &len, stdin);/* read a line from standard input */
+
+		nread = getline(&line, &len, stdin);
 		if (nread == -1)
 		{
 			if (feof(stdin))
 			{
-				printf("\n");
+				if (shell_state->is_interactive)
+					printf("\n");
 				break;
 			}
-			else
-			{
-				perror("getline");
-				exit(EXIT_FAILURE);
-			}
+			perror("getline");
+			shell_state->exit_status = EXIT_FAILURE;
+			break;
 		}
 		if (line[nread - 1] == '\n')
 			line[nread - 1] = '\0';
 		if (_strlen(line) == 0)
 			continue;
+		if (exit_command(line))
+			break;
 		if (_strcmp(line, "env") == 0)
 		{
 			env_command(envp);
 			continue;
 		}
-		if (exit_command(line))
-			break;
 		parse_args(line, args);
 		my_fork(args);
 	}
 	free(line);
-	return (EXIT_SUCCESS);
 }
