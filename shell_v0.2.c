d8615d2544de2ca815e6c2cf057ade5928777afb
Mathieu7483 <11436@holbertonstudents.com>
2025-08-20 16:14:25 +0200

    Update my_fork function to accept argv parameter and adjust related calls

--- shell_v0.2.c
+++ shell_v0.2.c
@@ -1,15 +1,18 @@
 #include "simple_shell.h"
 /**
- * my_fork - personalized fork function to execute a command
- * @args: array of arguments for the command
- * @envp: array of environment variables
- * Return: void
- */
-void my_fork(char **args, char **envp)
+* my_fork - personalized fork function to execute a command
+* @args: array of arguments for the command
+* @argv: array of arguments from main function
+* @envp: array of environment variables
+* Return: void
+*/
+void my_fork(char **args, char **argv, char **envp)
 {
 	pid_t pid;
 	int status;
+
 	char *path_env = my_getenv("PATH");
+
 	char *full_path;
 
 	if (args == NULL || args[0] == NULL)
@@ -18,7 +21,7 @@ void my_fork(char **args, char **envp)
 	full_path = _which(args[0], path_env);
 	if (full_path == NULL)
 	{
-		fprintf(stderr, "./simple_shell: 1: %s: not found\n", args[0]);
+		fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
 		return;
 	}
 
@@ -30,32 +33,32 @@ void my_fork(char **args, char **envp)
 		return;
 	}
 
-	if (pid == 0)
+	if (pid == 0) /* Child process */
 	{
 		if (execve(full_path, args, envp) == -1)
 		{
-			perror("execve");
+			perror(argv[0]);
 			free(full_path);
 			exit(EXIT_FAILURE);
 		}
 	}
-	else
+	else /* Parent process */
 	{
-		wait(&status);
+		waitpid(pid, &status, 0);
+		free(full_path);
 	}
-
-	free(full_path);
 }
 
 /**
- * parse_args - tokenize a command line into arguments
- * @line: input command line string
- * @args: array to store the resulting arguments
- * Return: number of arguments parsed
- */
+* parse_args - tokenize a command line into arguments
+* @line: input command line string
+* @args: array to store the resulting arguments
+* Return: number of arguments parsed
+*/
 int parse_args(char *line, char **args)
 {
 	int i = 0;
+
 	char *token;
 
 	token = strtok(line, " ");
