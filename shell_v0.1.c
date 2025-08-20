9d90e7d18c435545c0a835560b227b9068170548
Mathieu7483 <11436@holbertonstudents.com>
2025-08-20 11:16:51 +0200

    Refactor shell_v0.1.c by removing unnecessary includes and improving code structure for clarity

--- shell_v0.1.c
+++ shell_v0.1.c
@@ -1,5 +1,3 @@
-#include "simple_shell.h"
-
 /**
 * execute_command - create child and execute program
 * @command: command to execute
@@ -11,11 +9,9 @@ void execute_command(char *command, char **envp)
 {
 	pid_t pid;
 	int status;
-
 	char *argv[2];
 
 	pid = fork(); /* create child */
-
 	if (pid == -1)
 	{
 		perror("fork"); /* error if child doesn't work */
@@ -34,4 +30,54 @@ void execute_command(char *command, char **envp)
 	{
 		waitpid(pid, &status, 0); /* Wait for child process to complete */
 	}
+}
+/**
+* main - entry point for the simple shell program
+* @argc: number of arguments
+* @argv: array of argument strings
+* @envp: array of environment variables
+*
+* Return: 0 on success, 1 on error
+*/
+int main(int argc, char **argv, char **envp)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	char *args[256];
+	(void)argc, (void)argv;
+	while (1)
+	{
+		printf("cisfun$ "); /* display the prompt */
+		fflush(stdout);
+		nread = getline(&line, &len, stdin);/* read a line from standard input */
+		if (nread == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			else
+			{
+				perror("getline");
+				exit(EXIT_FAILURE);
+			}
+		}
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
+		if (_strlen(line) == 0)
+			continue;
+		if (_strcmp(line, "env") == 0)
+		{
+			env_command(envp);
+			continue;
+		}
+		if (exit_command(line))
+			break;
+		parse_args(line, args);
+		my_fork(args);
+	}
+	free(line);
+	return (EXIT_SUCCESS);
 }
